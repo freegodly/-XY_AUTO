28f6bd206f5d6d0efcc45b81030f60c5697ce6dd
freegodly <745364760@qq.com>
2016-07-30 01:36:02 +0800

    x

--- src/test.py
+++ src/test.py
@@ -97,8 +97,13 @@ def find_contours(img):
 
 import win32gui
 from PIL import ImageGrab
+from PIL import Image
 import win32con
 import pythoncom, pyHook
+
+
+screen_image = None
+
 def get_window(classname):
     hwnd = win32gui.FindWindow(classname, None)
     win32gui.ShowWindow(hwnd, win32con.SW_RESTORE) 
@@ -106,14 +111,32 @@ def get_window(classname):
     return hwnd
 
 def get_window_image(hwnd):
+    global screen_image
     game_rect = win32gui.GetWindowRect(hwnd)
+    client_rect = win32gui.GetClientRect(hwnd)
+    title_h =  game_rect[3] - game_rect[1] - client_rect[3] 
+    game_rect = (game_rect[0],game_rect[1]+title_h,game_rect[2],game_rect[3])
     src_image = ImageGrab.grab(game_rect)
+    screen_image = src_image
     #src_image.show()
     open_cv_image = np.array(src_image)
     open_cv_image = cv2.cvtColor(open_cv_image, cv2.cv.CV_BGR2RGB)
     #plt.imshow(open_cv_image)
     #plt.show()
     return open_cv_image
+
+def get_screen_sub_pilimage(x,y,w,h):
+    global screen_image
+    bounds = (x,y,x+w,y+h)
+    sub_img = screen_image.crop(bounds)
+    return sub_img
+
+def pil_to_cv2(image):
+    open_cv_image = np.array(image)
+    open_cv_image = cv2.cvtColor(open_cv_image, cv2.cv.CV_BGR2RGB)
+    return open_cv_image
+  
+
 
 def KeyStroke(event): 
     if str(event.Key)=='F12':
@@ -153,7 +176,27 @@ def find_title(img2):
     cv2.rectangle(vis, (x1, y1), (x2, y2), color, 2)
     return vis
 
+def get_ROI_image(image,rc):
+    image1M = cv.fromarray(image)  
+    image1Ip = cv.GetImage(image1M)  
+    cv.SetImageROI(image1Ip,rc)  
+    imageCopy = cv.CreateImage((rc[2], rc[3]),cv2.IPL_DEPTH_8U, 3)  
+    cv.Copy(image1Ip,imageCopy)  
+    cv.ResetImageROI(image1Ip)  
+    cv2image = np.asarray(cv.GetMat(imageCopy)) 
+    return  cv2image
+
 def image_despose(image):
+    # 坐标
+
+    rol_image = get_screen_sub_pilimage(20, 20,110,18)
+    title_image = pil_to_cv2(rol_image)
+    cv2.imwrite("point.jpg", title_image)
+    color = (0, 255, 0)
+    cv2.rectangle(image, (20, 20), (130, 38), color, 1)
+
+    return image
+
 
     image=cv2.imread('point.png')
     image=cv2.resize(image,(800,200),interpolation=cv2.INTER_AREA)
@@ -243,41 +286,189 @@ def image_match(img1,img2):
     cv2.rectangle(vis, (x1, y1), (x2, y2), color, 2)
     return vis
 
-def hist_similar(lh, rh):
 
-    assert len(lh) == len(rh)
-    return sum(1 - (0 if l == r else float(abs(l - r))/max(l, r)) for l, r in zip(lh, rh))/len(lh)
+def read_feature_file(filename):
 
-def comparehits(image):
+    feature_info = []
+
+    file_object = open(filename,'r')
+    try:
+        for line in file_object:
+            line = line.strip('\n')
+            sl = line.split(" ")
+            feature_info.append((sl[0],sl[1]))
+    finally:
+        file_object.close()
+
+    return feature_info
+
+
+def comparehits_min(image,featureimage):
     h, w = image.shape[:2]
-    fer = cv2.imread('tao.png')
-    h_f, w_f = fer.shape[:2]
+    h_f, w_f = featureimage.shape[:2]
 
-    image1M = cv.fromarray(image)
+    bins = w_f
+
+    # image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # fer_gray = cv2.cvtColor(featureimage, cv2.COLOR_BGR2HSV)
+
+    fer_gray = cv2.cvtColor(featureimage,cv2.COLOR_BGR2GRAY) 
+    image_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) 
+
+
+    hist_fer = cv2.calcHist([fer_gray],[0],None,[bins],[0,255.0]) 
+    #hist_fer = cv2.normalize(hist_fer).flatten()
+
+
+    image1M = cv.fromarray(image_gray)
     image1Ip = cv.GetImage(image1M)
-
-   
-    hist_fer = cv2.calcHist([fer],[0],None,[w_f],[200,255.0]) 
-   
-    fer1 = cv2.imread('0.png')
-    h_f, w_f = fer1.shape[:2]
-    hist_fer1 = cv2.calcHist([fer1],[0],None,[w_f],[200,255.0]) 
-
-    for i in xrange(int((w-154)/w_f)):
-        rc = (i*w_f+154, 25, w_f, h_f)
+    
+    bc_min = 1
+    start_px = -1 
+    for i in xrange(int(w-w_f)):
+        rc = (i, 0, w_f, h_f)
         #rc = (0, 0, w_f, h_f)
+        cv.SetImageROI(image1Ip,rc)  
+        imageCopy = cv.CreateImage((rc[2], rc[3]),cv2.IPL_DEPTH_8U,1)  
+        cv.Copy(image1Ip,imageCopy)  
+        cv.ResetImageROI(image1Ip) 
+        simg = np.asarray(cv.GetMat(imageCopy))
+       
+        cv2.imwrite("tmp.jpg", simg)
+        sub_img = cv2.imread("tmp.jpg")
+        hist_sub = cv2.calcHist([sub_img],[0],None,[bins],[0,255.0]) 
+        #hist_sub = cv2.normalize(hist_sub).flatten()
+       
+        #相关：CV_COMP_CORREL    
+        #卡方：CV_COMP_CHISQR
+        #直方图相交：CV_COMP_INTERSECT
+        # Bhattacharyya距离：CV_COMP_BHATTACHARYYA
+        inter = cv2.compareHist(hist_fer, hist_sub, cv2.cv.CV_COMP_BHATTACHARYYA)
+        if( inter < bc_min):
+            bc_min = inter
+            start_px = i
+       
+    return start_px
+
+def comparehits(image,featureimage,startx = 0,endx=0):
+    h, w = image.shape[:2]
+    h_f, w_f = featureimage.shape[:2]
+
+    fer_gray = cv2.cvtColor(featureimage,cv2.COLOR_BGR2GRAY) 
+    image_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) 
+
+    hist_fer = cv2.calcHist([fer_gray],[0],None,[w_f],[0,255.0]) 
+   
+
+
+    image1M = cv.fromarray(image_gray)
+    image1Ip = cv.GetImage(image1M)
+    
+    bc_min = 0.04
+    start_px = -1 
+    for i in xrange(int(w-w_f-endx+startx)):
+        rc = (i+startx, 0, w_f, h_f)
         cv.SetImageROI(image1Ip,rc)  
         imageCopy = cv.CreateImage((rc[2], rc[3]),cv2.IPL_DEPTH_8U, 1)  
         cv.Copy(image1Ip,imageCopy)  
         cv.ResetImageROI(image1Ip) 
         simg = np.asarray(cv.GetMat(imageCopy))
-      
-        hist_sub = cv2.calcHist([simg],[0],None,[w_f],[200,255.0]) 
-        #inter = cv2.compareHist(hist_fer, hist_sub, cv2.cv.CV_COMP_BHATTACHARYYA)
-        cv2.imwrite(str(i)+".png", simg)
-        print hist_similar(fer.real,hist_sub)
        
+        cv2.imwrite("tmp.jpg", simg)
+        sub_img = cv2.imread("tmp.jpg")
+        hist_sub = cv2.calcHist([sub_img],[0],None,[w_f],[0,255.0]) 
 
+        #相关：CV_COMP_CORREL    
+        #卡方：CV_COMP_CHISQR
+        #直方图相交：CV_COMP_INTERSECT
+        # Bhattacharyya距离：CV_COMP_BHATTACHARYYA
+        inter = cv2.compareHist(hist_fer, hist_sub, cv2.cv.CV_COMP_BHATTACHARYYA)
+        if( inter < bc_min):
+            start_px = i+startx
+            print inter
+            break
+       
+    return start_px
+
+
+
+def find_point(image):
+    mach_list = []
+    match_info = []
+
+    feature_point = read_feature_file("point.txt")
+    feature_map = read_feature_file("mapname.txt")
+
+    #先查找数字和汉字的分隔符
+    img_left = cv2.imread("left.png")
+    left_x = comparehits_min(image,img_left)
+    left_x =left_x-1
+    img_right = cv2.imread("right.png")
+    right_x = comparehits_min(image,img_right)
+
+    print "left:",left_x," right:",right_x
+
+    #匹配地图名字
+    for i in xrange(len(feature_map)):
+        img = cv2.imread(feature_map[i][0])
+        start_px = comparehits(image,img,0,left_x)
+        if start_px > -1 :
+           mach_list.append((start_px,feature_map[i][1]))
+           print start_px,":",feature_map[i][1].decode('utf-8')
+
+    mach_list.sort(cmp = lambda x ,y : cmp(x[0],y[0]))        
+    for m in mach_list:
+       match_info.append(m[1])
+
+    #匹配坐标
+    mach_list =[]
+    for i in xrange(len(feature_point)):
+        img = cv2.imread(feature_point[i][0])
+        start_px = comparehits(image,img,left_x+6,right_x)
+        if start_px > -1 :
+           mach_list.append((start_px,feature_point[i][1]))
+           print start_px,":",feature_point[i][1].decode('utf-8')
+    
+    mach_list.sort(cmp = lambda x ,y : cmp(x[0],y[0]))        
+    for m in mach_list:
+       match_info.append(m[1])
+   
+   
+    return match_info
+
+def split_point(title_image):
+    #get_screen_sub_pilimage()
+    #pil_to_cv2
+    #title_image = get_screen_sub_pilimage(20, 20,110,18)
+    #title_image = Image.open('point.jpg')
+    cv_title_image = pil_to_cv2(title_image)
+
+    h, w = cv_title_image.shape[:2]
+
+    img_left = cv2.imread("left.png")
+    left_x = comparehits_min(cv_title_image,img_left)
+    img_right = cv2.imread("right.png")
+    right_x = comparehits_min(cv_title_image,img_right) 
+
+    print "left:",left_x," right:",right_x
+
+    #切割汉字11个像素
+    endx = left_x - 2
+    while endx > 12:
+        bounds = (endx-11,0,endx,h)
+        sub_img = title_image.crop(bounds) 
+        sub_img.save("point/"+str(endx)+".png") 
+        print endx
+        endx = endx -11 -1
+
+    #切割数字5个像素
+    endx = right_x - 2
+    while endx-left_x > 6:
+        bounds = (endx-5,0,endx,h)
+        sub_img = title_image.crop(bounds) 
+        sub_img.save("point/s_"+str(endx)+".png") 
+        print endx
+        endx = endx -5 -1    
 
 if __name__ == '__main__':
 
@@ -287,13 +478,33 @@ if __name__ == '__main__':
     hm.HookKeyboard()
     #RCImageViewerFrame
     #WSGAME
-    hwnd = get_window("RCImageViewerFrame")
-    #hwnd = get_window("WSGAME")
+    #hwnd = get_window("RCImageViewerFrame")
+    hwnd = get_window("WSGAME")
 
 
-    open_cv_image = get_window_image(hwnd)
-    img = image_despose(open_cv_image) 
-    comparehits(img)
+    #open_cv_image = get_window_image(hwnd)
+    #img = image_despose(open_cv_image)
+    title_image1 = Image.open('point.jpg')
+    title_image2 = Image.open('point1.png')
+    split_point(title_image1)
+    split_point(title_image2)
+    exit()
+    
+    img = cv2.imread('point.jpg')
+    fer_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
+
+    hist_fer = cv2.calcHist([fer_gray],[0],None,[1100],[0,255]) 
+    plt.figure()
+    plt.plot(range(len(hist_fer)),hist_fer)
+    plt.show()
+    exit()
+    #comparehits(img)
+    mach_list = find_point(img)
+    info = ""
+    for m in mach_list:
+        info+=m
+    #print x.decode('utf-8')
+    print info.decode('utf-8')
     cv2.imshow('frame',img)
     cv2.waitKey(0) 
     exit()
